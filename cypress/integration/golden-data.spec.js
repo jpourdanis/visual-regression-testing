ccd17250ef54ccc5970c3f7823927d483d66d9ed
Ιωάννης Πουρδάνης <ipourdanis@hotmail.com>
2020-02-17 16:01:53 +0200

    add specific cases

--- cypress/integration/golden-data.spec.js
+++ cypress/integration/golden-data.spec.js
@@ -1,8 +1,33 @@
 describe('Golden Data', function() {
+
+    before(function(){
+        cy.visit(Cypress.env('host'))
+    })
+
+
     it('Visual Regression Test', function() {
       
-      cy.visit(Cypress.env('host'))
-      //cy.get('body > div > table > tbody > tr:nth-child(3) > td:nth-child(4)').contains('@twitter');
-      cy.compareSnapshot('All-Page-Snapshot', 0.0);
+      //We can set option the error threshold : Value can range from 0.00 (no difference) to 1.00 (every pixel is different)
+      cy.compareSnapshot('full-page-snapshot', 0.1);
     })
+
+    it('Specific cell with changed color', function() {
+      
+        cy.get('#row-4-surname').compareSnapshot('color-change')
+
+    })
+
+    it('Specific cell with changed font', function() {
+      
+        cy.get('#row-2-surname').compareSnapshot('font-change')
+
+    })
+
+    it('Specific cell with changed value', function() {
+      
+        cy.get('#row-9-firstname').compareSnapshot('value-change')
+
+    })
+    
+
   })
