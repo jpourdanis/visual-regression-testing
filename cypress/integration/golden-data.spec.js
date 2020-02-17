a0278828065d510f72e6efc9aeb6c683e93fd0ef
Ιωάννης Πουρδάνης <ipourdanis@hotmail.com>
2020-02-17 14:31:48 +0200

    added host enviroment variable

--- cypress/integration/golden-data.spec.js
+++ cypress/integration/golden-data.spec.js
@@ -1,6 +1,7 @@
 describe('Golden Data', function() {
     it('Visual Regression Test', function() {
-      cy.visit('http://localhost:8081/')
+      
+      cy.visit(Cypress.env('host'))
       //cy.get('body > div > table > tbody > tr:nth-child(3) > td:nth-child(4)').contains('@twitter');
       cy.compareSnapshot('All-Page-Snapshot', 0.0);
     })
