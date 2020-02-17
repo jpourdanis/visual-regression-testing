ee16d54c5b37b657ff6ac1126de5fcc9361a441c
Ιωάννης Πουρδάνης <ipourdanis@hotmail.com>
2020-02-17 13:28:49 +0200

    added cypress visual regression package

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/src/golden-data.html');
 })
 
+app.get('/vrtest', function (req, res) {
+   
+    res.sendFile(__dirname + '/src/false-data.html');
+})
+
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
